d8c29c4aaf170d485cd65a172298c88d4478c4f6
apizzimenti <pizzimentianthony@gmail.com>
2021-01-26 09:53:04 -0500

    modify docstring to reflect what actually gets passed to the function

--- src/components/Charts/ChangePopulationDataset.js
+++ src/components/Charts/ChangePopulationDataset.js
@@ -1,15 +1,20 @@
 
 import Select from "../Select";
+import Parameter from "../Parameter";
 
+const descriptiveNames = {
+    "Population": "2010 Census",
+    "Population (2018)": "2018 ACS"
+};
 
 /**
  * Handler for the Select dropdown menu. Consumes an Editor object to re-assign
  * its child State object's active Population, and forces the Editor to
  * re-render plugins when a different available Population dataset is selected.
- * @param {Editor} editor editor object passed from the population balance
+ * @param {Editor} editor Editor instance passed from the population balance
  * plugin.
  * @param {Population[]} dataset Population datasets available to the user.
- * @returns {function(*): void} Callback function.
+ * @returns {function(*): void} Callback.
  * @private
  */
 function _onDatasetChange(editor, dataset) {
@@ -34,13 +39,23 @@ function _onDatasetChange(editor, dataset) {
  * the active dataset based on which one is selected.
  * @param {Editor} editor Editor instance passed from the population balance
  * plugin.
- * @returns {str} Renderable HTML containing the dropdown menu.
+ * @returns {Parameter} Renderable HTML containing the dropdown menu.
  */
 const changePopulationDataset = editor => {
-    return Select(
-        editor.state.datasets,
-        _onDatasetChange(editor, editor.state.datasets)
-    );
+    // Modify the names to make them more descriptive.
+    editor.state.datasets.forEach(pop => {
+        if (descriptiveNames.hasOwnProperty(pop.name)) {
+            pop.name = descriptiveNames[pop.name];
+        }
+    });
+
+    return Parameter({
+        label: "Dataset",
+        element: Select(
+            editor.state.datasets,
+            _onDatasetChange(editor, editor.state.datasets)
+        )
+    });
 };
 
 export default changePopulationDataset;
